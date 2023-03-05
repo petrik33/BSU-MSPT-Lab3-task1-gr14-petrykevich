c880a15d136e849abb8612c946cb89061ba1716c
Timofey Petrikevich <petrik3003@gmail.com>
2023-03-05 19:09:42 +0000

    feat: print random array

--- .ccls-cache/@home@runner@BSU-MSPT-Lab3-task1-gr14-petrykevich/src@powerOfFive.c
+++ .ccls-cache/@home@runner@BSU-MSPT-Lab3-task1-gr14-petrykevich/src@powerOfFive.c
@@ -1,4 +1,6 @@
 #include "../include/powerOfFive.h"
+#include <stdlib.h>
+#include <time.h>
 
 int isPowerOfFive(int x) {
   do {
@@ -8,4 +10,17 @@ int isPowerOfFive(int x) {
     x /= 5;
   } while(x % 5 == 0 && x != 0);
   return 0;
+}
+
+int seed = 0;
+int* randomArray(int size) {
+  if(seed == 0) {
+    seed = time(NULL);
+    srand(seed);
+  }
+  int* arrPtr = (int*)malloc(size * sizeof(int));
+  for(int i = 0; i < size; i++) {
+    arrPtr[i] = rand();
+  }
+  return arrPtr;
 }
--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 /* main.c */
 #include <stdio.h>
+#include "../include/powerOfFive.h"
 
-void main (void)
+int main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	int size;
+	printf ("Enter array size: ");
+  scanf('%d', &size);
+
+  int* array = randomArray(size);
+  for(int i = 0; i < size; i++) {
+    printf('%d', array[i]);
+  }
+  
+  return 0;
 }
