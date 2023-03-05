79cd5a470f018073d3e45885a19550ca44fe1246
Timofey Petrikevich <petrik3003@gmail.com>
2023-03-05 19:47:18 +0000

    misc: replit nix store

--- .ccls-cache/@home@runner@BSU-MSPT-Lab3-task1-gr14-petrykevich/include@powerOfFive.h
+++ .ccls-cache/@home@runner@BSU-MSPT-Lab3-task1-gr14-petrykevich/include@powerOfFive.h
@@ -2,6 +2,6 @@
 #define _POWER_OF_FIVE_
 
 int isPowerOfFive(int x);
-int* randomArray(int size);
+int *randomArray(int size, int max);
 
 #endif
--- .ccls-cache/@home@runner@BSU-MSPT-Lab3-task1-gr14-petrykevich/src@main.c
+++ .ccls-cache/@home@runner@BSU-MSPT-Lab3-task1-gr14-petrykevich/src@main.c
@@ -6,12 +6,22 @@ int main (void)
 {
 	int size;
 	printf ("Enter array size: ");
-  scanf('%d', &size);
+  scanf("%d", &size);
 
-  int* array = randomArray(size);
-  for(int i = 0; i < size; i++) {
-    printf('%d', array[i]);
+  int max;
+  printf("Enter max value: ");
+  scanf("%d", &max);
+
+  int *array = randomArray(size, max);
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d", array[i]);
+    if (isPowerOfFive(array[i]))
+    {
+      printf(" is power of 5");
+    }
+    printf("\n");
   }
-  
+
   return 0;
 }
